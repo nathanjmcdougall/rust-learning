1c90334a2ff2ff228a0019802f4f45028d151f40
Nathan McDougall <nathan.j.mcdougall@gmail.com>
2025-12-21 21:28:55 +1300

    Narrow scope of intermediate variables

--- fibonacci/src/main.rs
+++ fibonacci/src/main.rs
@@ -1,16 +1,15 @@
 use std::io;
 
-const MAX_N: u64 = 93; // To avoid overflows
+const MAX_N: u64 = 94; // To avoid overflows
 
 fn main() {
     println!("Let's calculate the n-th Fibonacci number!");
 
     let mut n: u64;
-    let mut n_str: &str;
-    let mut input;
+    let ord_suffix: &str;
     loop {
         println!("Please enter n:");
-        input = String::new();
+        let mut input = String::new();
         match io::stdin().read_line(&mut input) {
             Ok(_) => (),
             Err(_) => {
@@ -18,6 +17,7 @@ fn main() {
                 continue;
             }
         };
+        let n_str: &str;
         n_str = input.trim();
         n = match n_str.parse() {
             Ok(n) => n,
@@ -31,21 +31,23 @@ fn main() {
             continue;
         }
 
+        ord_suffix = match n_str {
+            s if s.ends_with("11") || s.ends_with("12") || s.ends_with("13") => "th",
+            _ => match n_str.chars().next_back() {
+                Some('1') => "st",
+                Some('2') => "nd",
+                Some('3') => "rd",
+                _ => "th",
+            },
+        };
+
         break;
     }
 
     let fib = fibonacci(n);
     // .next() and .next_back() to access the first and last char of a double-ended
     // iterator trait
-    let ord_suffix = match n_str {
-        s if s.ends_with("11") || s.ends_with("12") || s.ends_with("13") => "th",
-        _ => match n_str.chars().next_back() {
-            Some('1') => "st",
-            Some('2') => "nd",
-            Some('3') => "rd",
-            _ => "th",
-        },
-    };
+
     println!("The {n}{ord_suffix} Fibonacci number is: {fib}");
 }
 fn request_new_choice() {
